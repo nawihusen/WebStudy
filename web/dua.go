4b86b925ee49626d26b12177a8dadc23bc500ece
“Nawi” <nawihusen@gmail.com>
2022-11-18 14:50:33 +0800

    +

--- web/dua.go
+++ web/dua.go
@@ -29,7 +29,7 @@ func Route21(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
-	t, er := template.ParseFiles("./temp/kedua/test.html")
+	t, er := template.ParseFiles("./temp/kedua/view.html")
 	if er != nil {
 		http.Error(w, er.Error(), 500)
 		fmt.Println(er.Error())
@@ -53,4 +53,24 @@ func (s Info) Perkenalan() string {
 
 func (s Info) Say(name string) string {
 	return "Where are you from ," + name + "?"
+}
+
+func Route22(w http.ResponseWriter, r *http.Request) {
+	person := Person{
+		Name:    "Husen",
+		Age:     21,
+		Hobbies: []string{"baca novel", "main game", "baca manga"},
+		Info: Info{
+			Address:  "Japan",
+			WhatsApp: "0808",
+		},
+	}
+
+	t, er := template.ParseFiles("./temp/kedua/baru.html")
+	if er != nil {
+		http.Error(w, er.Error(), 500)
+		fmt.Println(er.Error())
+		return
+	}
+	t.Execute(w, person)
 }
